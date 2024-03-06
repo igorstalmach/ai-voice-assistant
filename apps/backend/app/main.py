168b61a3fa8a3fc7418ac197b8dc192a3e03d2ad
Krystian Ogonowski <krystian@ogonowski.it>
2024-03-06 19:49:36 +0100

    rename package backend to app

---
+++ apps/backend/app/main.py
@@ -0,0 +1,14 @@
+from typing import  Annotated
+from fastapi import FastAPI, WebSocket, File
+app = FastAPI()
+
+@app.post("/speech")
+def speech(file: Annotated[bytes, File()]):
+  return {"file_size": len(file)}
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+  await websocket.accept()
+  while True:
+    data = await websocket.receive_text()
+    await websocket.send_text(f"Message text was: {data}")
