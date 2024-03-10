eb3bc67571c78570b36dd48b28642952b3ae11c5
Krystian Ogonowski <krystian@ogonowski.it>
2024-03-10 15:08:32 +0100

    Feat/initial tts (#10)
    * app config & first tts
    
    * Feat/ws-endpoint (#1)
    
    * create docker-compose config
    
    * rename package backend to app
    
    * add python-multipart for handling files
    
    * ws & file upload logic
    
    * add packages
    
    * update docs
    
    * update .env.exmaple & gitignore
    
    * remove console logs
    
    * resolve comment
    
    * more config

---
+++ README.md
@@ -0,0 +1,31 @@
+# PL TTS GPT wrapper
+
+## Description
+
+## Installation
+
+The app consists of two parts: the backend and the frontend.
+
+WIP
+
+### Backend (FastAPI)
+
+You must have docker installed to run the backend. Refer to [TODO]().
+
+`docker-compose build` to build the backend image.
+then
+`docker-compose up` to run the backend.
+
+The project is set to run on port `8000`.
+
+## Frontend (Expo)
+
+We use [pnpm](https://pnpm.io/) for simplicity and speed in working with monorepos. Refer to [pnpm docs](https://pnpm.io/installation) for installation instructions.
+
+since the app is mobile you have to set your local ip address in `EXPO_PUBLIC_API_URL` in `.env.local` file for development.
+
+### Docs (?)
+
+We may want to deploy the docs to github pages or smth.
+
+## WIP
---
+++ apps/mobile/app/_layout.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from 'react';
+import { Button, Text, SafeAreaView, StyleSheet, TextInput } from 'react-native';
+import * as Speech from 'expo-speech';
+import { useWebsocket, ws } from '../hooks/useWebsocket';
+import { useRecord } from '../hooks/useRecord';
+import { sendFile } from '../hooks/useFetch';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    paddingTop: 100,
+    gap: 16,
+  },
+});
+
+const inwokacja = `Litwo, Ojczyzno moja! ty jesteś jak zdrowie;
+Ile cię trzeba cenić, ten tylko się dowie,
+Kto cię stracił. Dziś piękność twą w całej ozdobie
+Widzę i opisuję, bo tęsknię po tobie.`;
+
+export default function RootLayout() {
+  const [text, setText] = useState(inwokacja);
+
+  // https://docs.expo.dev/versions/latest/sdk/localization/
+  const plVoices = [
+    'pl-pl-x-oda-local',
+    'pl-pl-x-bmg-local',
+    'pl-pl-x-oda-network',
+    'pl-pl-x-afb-network',
+    'pl-pl-x-zfg-network',
+    'pl-pl-x-jmk-network',
+    'pl-pl-x-bmg-network',
+    'pl-pl-x-zfg-local',
+    'pl-pl-x-jmk-local',
+    'pl-pl-x-afb-local',
+    'pl-PL-language',
+  ];
+
+  useWebsocket();
+  const { stopRecording, startRecording, recording } = useRecord();
+
+  const speakNext = (index: number = 0) => {
+    const voiceIdx = Math.floor(Math.random() * plVoices.length);
+    Speech.speak(inwokacja.split('.')[index], {
+      language: 'pl', // TODO: maybe add localization
+      rate: 1,
+      voice: plVoices[voiceIdx],
+      onDone: () => speakNext(index + 1),
+    });
+  };
+
+  const sendMessage = () => {
+    ws.send('Hello');
+  };
+
+  const handleAudioPress = async () => {
+    if (recording) {
+      const uri = await stopRecording();
+      const res = await sendFile(uri);
+
+      const fileSize = JSON.parse(res.body).file_size;
+      Speech.speak(`Wysłano plik o rozmiarze ${Math.floor(fileSize / 1000)} kilobajtów`, {
+        language: 'pl',
+        rate: 1,
+        voice: plVoices[0],
+      });
+    } else {
+      startRecording();
+    }
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Input: {text}</Text>
+      <TextInput
+        placeholder="Type here to translate!"
+        value={text}
+        onChangeText={(e) => setText(e)}
+      />
+      <Button title="DUPA" onPress={() => speakNext()} />
+      <Button title="Stop" onPress={() => Speech.stop()} />
+      <Button title="Send" onPress={sendMessage} />
+
+      <Button title={`${recording ? 'Stop' : 'Start'} recording`} onPress={handleAudioPress} />
+    </SafeAreaView>
+  );
+}
